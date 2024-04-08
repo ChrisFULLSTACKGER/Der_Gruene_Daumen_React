5c2dc724e0ff4813c3e3221aadbd08a215b3ce13
Christopher Pollow <>
2024-04-08 16:28:34 +0200

    Routing Home component in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import './App.css';
 import classNames from "classnames"
-import Header from "./Header";
-import Logo from "./Logo";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
-import Content from "./Content/ContentMainComponent"
-
-import Hello from "./Content/ContentTestComponentHello";
-import Goodbye from "./ContentTestComponentGoodbye";
+import Home from './Routes/Home'
 
 const App = () => {
   return (
-    <div className="App">
-        {/*<Router>*/}
-        {/*    <Routes>*/}
-        {/*        <Route exact path="/" element={<Hello/>}/>*/}
-        {/*        <Route path='/goodbye' element={<Goodbye/>}/>*/}
-        {/*    </Routes>*/}
-        {/*</Router>*/}
-       <Header/>
-        <Logo />
-        <Navbar/>
-        <Content/>
-        <Footer/>
-    </div>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+      </Routes>
   );
-}
+};
 
 export default App;
---
+++ src/Routes/Home/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import classNames from "classnames"
+import Header from "../../Header";
+import Logo from "../../Logo";
+import Footer from "../../Footer";
+import Navbar from "../../Navbar";
+import Content from "../../Content/ContentMainComponent";
+
+const Home = () => {
+    return (
+        <div className='App'>
+            <Header/>
+            <Logo />
+            <Navbar/>
+            <Content/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Home;
